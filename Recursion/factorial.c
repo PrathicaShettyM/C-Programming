05f865055bf2fbc8979ea041a1210c97bbf98546
Prathica Shetty M <123286880+PrathicaShettyM@users.noreply.github.com>
2024-01-06 15:55:02 +0530

    factorial of a number

--- Recursion/factorial.c
+++ Recursion/factorial.c
@@ -1,5 +1,14 @@
 #include<stdio.h>
+int factorial(int n){
+    if(n == 0 || n==1 ) return 1;
+    return n*factorial(n-1);
+}
 int main(){
-    
+    int n;
+    printf("Enter a number : ");
+    scanf("%d", &n);
+    int res = factorial(n);
+    printf("%d", res);
+
     return 0;
 }
